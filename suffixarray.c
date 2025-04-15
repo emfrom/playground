6811307f8a7b4a209c7d1492bb63c6fa4457e3de
emfrom <vertlaforet@protonmail.com>
2025-04-15 04:35:39 +0200

    Changed data types, start new algo

--- suffixarray.c
+++ suffixarray.c
@@ -5,67 +5,48 @@
 
 #define TEST_SUFFIXARRAY
 
-#if UINT64_MAX < SIZE_MAX
-#error "size doesnt fit in uint64"
-#endif
-
-#if PTR_MAX > UINT64_T
-#error "ptr bigger than u64"
-#endif
-
-#if 256 < UCHAR_MAX
-#error "uchar size assumption failed"
-#endif
-
-
-/**
-   - This is an initial naive sort
-   - Can only handle strings, not binary data
-*/
-
 #include "xmalloc.c"
 
 struct suffixarray_s {
-  uint64_t length;
-  uint64_t *memb;
+  int length;
+  int *index;
 };
 
 typedef struct suffixarray_s *suffixarray;
 
-int string_compare(const void *pointer1, const void *pointer2) {
-  char *string1 = *(char **) pointer1;
-  char *string2 = *(char **) pointer2;
-
-  return strcmp(string1, string2);
-}
-
 suffixarray suffixarray_create(char *data, size_t length) {
 
-  //Create substrings
-  char **temp_storage = xmalloc(sizeof(char *) * length);
-  for(uint64_t i = 0; i < length ; i++)
-    temp_storage[i] = data + i;
-
-  //Sort substrings
-  qsort(temp_storage, length, sizeof(char *), string_compare);
-  
   //Create suffixarray
   suffixarray retval;
   retval = xmalloc(sizeof(struct suffixarray_s));
   retval->length = length;
-  retval->memb = xmalloc(sizeof(uint64_t) * length);
+  retval->index = xmalloc(sizeof(int) * length);
+
+  //Create substrings indices
+  for(int i = 0; i < length ; i++)
+    retval->index[i] = i;
   
-  for(uint64_t i = 0; i < length ; i++)
-    retval->memb[i] = (uint64_t) (temp_storage[i] - data);
+  /**
+   * Prefix sort O(n log n)
+   *
+   * Prefix sort exploints symmetries in the data sort faster
+   *
+   * Assumptions/definitions:
+   *
+   * - Let "rank" be value of char, same char has same rank. TODO: 0 and out of bounds ranks?
+   * - 
+   */
 
+  // Initial ranking (with sort)
 
-  xfree(temp_storage);
+  // -- Loop over comparison lenghts (start 1)
+
 
   return retval; 
 }
 
 void suffixarray_destroy(suffixarray sa) {
-  free(sa->memb);
+  free(sa->index);
   free(sa);
 }
 
@@ -85,8 +66,7 @@ int main(int arg_count, char **arg_vector) {
   suffixarray sa = suffixarray_create(string, len);
 
   for(int i = 0; i < sa->length ; i++)
-    printf("%d: %s\n", i, string + sa->memb[i]);
-				      
+    printf("%d: %s\n", sa->index[i], string + sa->index[i]);
 
   suffixarray_destroy(sa);
   
