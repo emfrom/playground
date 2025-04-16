04f48dbf1831e3eafd1f705219cdd2b48f3a4f2e
emfrom <vertlaforet@protonmail.com>
2025-04-16 07:52:07 +0200

    Finished suffix array impl

--- suffixarray.c
+++ suffixarray.c
@@ -2,7 +2,6 @@
 #define SUFFIXARRAY_C
 
 /*
-  TODO: Sorting by ranks do not work, needs differently written radix sort
   TODO: Generating ranks do not work, needs a new function
 */
 
@@ -21,78 +20,14 @@
 struct suffixarray_s {
   size_t length;
   uint8_t *data;
-  uint64_t *indices;
+  size_t *indices;
 };
 
 typedef struct suffixarray_s *suffixarray;
 
-//For the inital ranking of the suffixes
-uint16_t get_initial(uint64_t index, void *base, void *userdata) {
-  suffixarray sa = base;
-
-  if(index + 1 == sa->length)
-    return sa->data[index] << 8;
-
-  return sa->data[index] << 8 | sa->data[index + 1];
-}
-
-//Data needed to generate sorting tuples (i.e. u16 from 2xu8)
-struct rank_data_s {
-  uint64_t *ranks;
-  uint64_t prefix_length;
-};
-
-/*
-  sa   = [0, 2, 4, 3, 5, 1]
-  rank = [1, 3, 1, 2, 1, 2]
-
-sa[i] suffix   rank[sa[i]]  rank[sa[i]+1]
- 0   acabab       1         3    
- 2     abab       1         2
- 4       ab       1         2
- 3      bab       2         1
- 5        b       2         0
- 1    cabab       3         1
- */
-uint16_t get_rank_tuple(uint64_t i, void *base, void *userdata) {
-  suffixarray sa = base;
-  struct rank_data_s *rank_data = userdata;
-  uint16_t tuple;
-
-  //TODO: BUG: uint16 only works for up to 256 char lenght (max rank == length)
-  tuple = (( rank_data->ranks[sa->indices[i]]) & 0x0F) << 8;
-    
-  uint64_t k = i + rank_data->prefix_length;
-
-  if(k >= sa->length)
-    return tuple;
-
-  //TODO: BUG: uint16 only works for up to 256 char lenght (max rank == length)
-  return tuple | (rank_data->ranks[sa->indices[i]] & 0x0F);
-}
 
 
-//Convert ordered indexes into ranks
-uint64_t *rank_from_index(suffixarray sa, int *sorted) {
-  uint64_t *ranks = xmalloc(sizeof(uint64_t)*sa->length);
 
-  uint64_t current_rank = 1; //start at 1
-  ranks[0] = 1;
-
-  for(uint64_t i = 1; i < sa->length ; i++) {
-    //TODO: BUG: This doesnt work (how did I come up with this?)
-    if(sa->data[sa->indices[i]] != sa->data[sa->indices[i - 1]])
-       current_rank += 1;
-
-    ranks[i] = current_rank;
-  }
-
-  //All elements in the rank array have a unique rank
-  // implies last rank == lenght
-  *sorted = current_rank == sa->length ? 1 : 0;
-    
-  return ranks;
-}
 
 suffixarray suffixarray_create(char *data, size_t length) {
 
@@ -101,25 +36,22 @@ suffixarray suffixarray_create(char *data, size_t length) {
   sa = xmalloc(sizeof(struct suffixarray_s));
   sa->length = length;
   sa->data = (uint8_t *)data;
-  
-  /**
-   * Prefix sort O(n log n), with radix or counting sort 
-   *
-   * Prefix sort exploints symmetries in the data sort faster
-   *
-   * Assumptions/definitions:
-   */
-  sa->indices = rsort_index(sa, length, get_initial, NULL);
-  
-  
-  
-  // Initial ranking 
-     
 
-  //List is now sorted and ranked
+  //Create substrings
+  char **suffixes = xmalloc(sizeof(char *)*length);
+  for(size_t i=0; i<length; i++)
+    suffixes[i] = data + i;
 
+  //Sort all the strings O(n)
+  radixsort_asciz(suffixes, length, 0);
+
+  //Store indices
+  sa->indices = xmalloc(sizeof(size_t) * length);
+  for(size_t i; i < length; i++)
+    sa->indices[i] = suffixes[i] - data;
+    
+  free(suffixes);
   
-
   return sa; 
 }
 
